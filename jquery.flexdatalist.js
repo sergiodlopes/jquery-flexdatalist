2b5dcc3ee6b5266b474d3f8c022d0197c7de9a9a
Sergio Dinis Lopes <sergiodlopes@gmail.com>
2018-01-08 23:22:20 +0000

    prevent plugin to autodiscover/load the input

--- jquery.flexdatalist.js
+++ jquery.flexdatalist.js
@@ -3,7 +3,7 @@
  * Autocomplete input fields, with support for datalists.
  *
  * Version:
- * 2.2.2.1
+ * 2.2.2.2
  *
  * Depends:
  * jquery.js > 1.8.3
@@ -1912,7 +1912,7 @@ jQuery(function ($) {
         }).data('flexdatalist', true);
     }
 
-    jQuery('input.flexdatalist:not(.flexdatalist-set)').flexdatalist();
+    jQuery('input.flexdatalist:not(.flexdatalist-set):not(.autodiscover-disabled)').flexdatalist();
 });
 
 (function ($) {
